1621d4f5ead4b1aa2724544c5c7560823a453a87
AMM. Hathil Thahee <146162318+Thahee02@users.noreply.github.com>
2026-02-19 00:46:38 +0530

    Security: Restrict the frontend request url (#2)
    * Created: CRUD API for Place and Category
    
    * Security: Restrict the frontend request url

--- src/main/java/com/atvisited/atvisited/category/CategoryController.java
+++ src/main/java/com/atvisited/atvisited/category/CategoryController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/categories")
-@CrossOrigin(origins = "http://localhost:5173")
 public class CategoryController {
 
     @Autowired
--- src/main/java/com/atvisited/atvisited/place/PlaceController.java
+++ src/main/java/com/atvisited/atvisited/place/PlaceController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/places")
-@CrossOrigin(origins = "http://localhost:5173")
 public class PlaceController {
 
     @Autowired
